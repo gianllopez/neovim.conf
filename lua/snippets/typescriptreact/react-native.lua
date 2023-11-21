9c9759365d6b0f8a0417cf1e83dcfc586206289f
Gian López <lopezarizagianlucas@gmail.com>
2023-11-20 22:39:52 -0500

    [refactor]: agregado nuevo `snippet`

--- lua/snippets/typescriptreact/react-native.lua
+++ lua/snippets/typescriptreact/react-native.lua
@@ -1,5 +1,10 @@
+local function capitalize(args, parent, user_args)
+  local string = args[1][1]
+  return (string:gsub("^%l", string.upper))
+end
+
 return {
-  -- "React Native" screen with "react-native-safe-area-context" and "nativewind" functionalities
+  -- `React Native` screen with general and standarized functionalities.
   s("rns", {
     t({
       "import { SafeAreaView } from 'react-native-safe-area-context';",
@@ -21,5 +26,47 @@ return {
       "};",
     }),
   }),
+  -- `React Native` screen with an `useEffect` call on component mount.
+  s("rnsa", {
+    t({
+      "import { useEffect, useState } from 'react';",
+      "import { SafeAreaView } from 'react-native-safe-area-context';",
+      "",
+      "export function ",
+    }),
+    i(1),
+    t({
+      "() {",
+      "\tconst [",
+    }),
+    i(2),
+    t(", set"),
+    f(capitalize, { 2 }),
+    t({ "] = useState<" }),
+    i(3),
+    t(">("),
+    i(4),
+    t({
+      ");",
+      "",
+      "\tuseEffect(() => {",
+      "\t\t",
+    }),
+    i(5),
+    t({
+      "",
+      "\t}, []);",
+      "",
+      "\treturn (",
+      '\t\t<SafeAreaView className="flex-1 p-4 bg-white">',
+      "\t\t\t",
+    }),
+    i(6),
+    t({
+      "",
+      "\t\t</SafeAreaView>",
+      "\t);",
+      "};",
+    }),
+  }),
 }
-
