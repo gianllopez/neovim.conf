f86782bc055c551057ce5225017530821ce85dab
Gian López <lopezarizagianlucas@gmail.com>
2023-12-06 12:00:27 -0500

    [feature]: agregado `indent-blankline`

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,36 +1,38 @@
 return {
-	"jose-elias-alvarez/null-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+  "jose-elias-alvarez/null-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
 
-		local formatting = null_ls.builtins.formatting
+    local formatting = null_ls.builtins.formatting
 
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-		local sources = {
-			-- lua
-			formatting.stylua,
-			-- javascript / typescript
-			formatting.prettier,
-			-- python
-			formatting.black,
-			formatting.isort,
-		}
+    local sources = {
+      -- lua
+      formatting.stylua,
+      -- javascript / typescript
+      formatting.prettier,
+      -- python
+      formatting.black,
+      formatting.isort,
+      -- dart
+      formatting.dart_format,
+    }
 
-		null_ls.setup({
-			sources = sources,
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ bufnr = bufnr })
-						end,
-					})
-				end
-			end,
-		})
-	end,
+    null_ls.setup({
+      sources = sources,
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+          })
+        end
+      end,
+    })
+  end,
 }
--- lua/plugins/nvim-treesiter.lua
+++ lua/plugins/nvim-treesiter.lua
@@ -1,41 +1,42 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "lua",
-        "python",
-        "typescript",
-        "tsx",
-        "html",
-        "css",
-        "scss",
-      },
-      autotag = {
-        enable = true,
-      },
-    })
-  end,
-  dependencies = {
-    {
-      "numToStr/Comment.nvim",
-      lazy = false,
-      opts = {},
-      dependencies = {
-        { "JoosepAlviste/nvim-ts-context-commentstring" },
-      },
-      config = function()
-        require("Comment").setup({
-          pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-        })
-      end,
-    },
-    {
-      "kevinhwang91/nvim-ufo",
-      dependencies = { "kevinhwang91/promise-async" },
-      opts = {},
-    },
-    { "windwp/nvim-ts-autotag" },
-  },
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"lua",
+				"python",
+				"typescript",
+				"tsx",
+				"html",
+				"css",
+				"scss",
+				"dart",
+			},
+			autotag = {
+				enable = true,
+			},
+		})
+	end,
+	dependencies = {
+		{
+			"numToStr/Comment.nvim",
+			lazy = false,
+			opts = {},
+			dependencies = {
+				{ "JoosepAlviste/nvim-ts-context-commentstring" },
+			},
+			config = function()
+				require("Comment").setup({
+					pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+				})
+			end,
+		},
+		{
+			"kevinhwang91/nvim-ufo",
+			dependencies = { "kevinhwang91/promise-async" },
+			opts = {},
+		},
+		{ "windwp/nvim-ts-autotag" },
+	},
 }
